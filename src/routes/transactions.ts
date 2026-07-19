e48160dc196e4d56faa33f046a6257f96ce4c0f7
Daniel <danieldcc.colares@gmail.com>
2026-07-19 12:06:03 -0300

    chore: list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { z } from "zod"
 import { knex } from "../database.ts"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (_request, reply) => {
+    const transactions = await knex('transactions').select('*')
+
+    return reply.status(200).send({ transactions })
+  })
+
+  app.get("/:id", async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.uuid()
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex("transactions").where('id', id).first()
+
+    return reply.status(200).send({ transaction })
+  })
+
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string().min(3, { error: "Deve ser maior que 3 caracteres" }),
