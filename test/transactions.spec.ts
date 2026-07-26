b3682c15ec6960c2da92722a0c2eb3d11f23d687
Daniel <danieldcc.colares@gmail.com>
2026-07-26 11:20:20 -0300

    test: add list of transactions test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -6,18 +6,42 @@ describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
-})
 
-afterAll(async () => {
-  await app.close()
-})
-
-it('should be able to create a new transaction', async () => {
-  const response = await request(app.server).post('/transactions').send({
-    title: 'New transaction',
-    amount: 5000,
-    type: 'credit',
+  afterAll(async () => {
+    await app.close()
   })
 
-  expect(response.statusCode).toEqual(201)
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') ?? []
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
