467671b0b9927e6aedee875ef3ca470e012cc197
Daniel <danieldcc.colares@gmail.com>
2026-07-26 11:58:18 -0300

    test: config db for tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,12 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
 //NODE_ENV: development, test, production
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
