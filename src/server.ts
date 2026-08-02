7f2a0b3ff0238d3a3b587e714bceeb961556c016
Daniel <danieldcc.colares@gmail.com>
2026-08-02 12:24:45 -0300

    chore: add host to the listen call

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app.ts'
 import { env } from './env/index.ts'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP Server running on port 3333!')
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`HTTP Server running on port ${env.PORT}`)
 })
